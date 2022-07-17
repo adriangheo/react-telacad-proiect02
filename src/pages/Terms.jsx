37235f2da6d9aa0c8cc34870aec46342af979a48
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-07-17 17:36:52 +0300

    add Terms and Condions page, plus route plus links

---
+++ src/pages/Terms.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+function Terms() {
+    return(
+        <div>
+            <Layout>
+                <h1>Terms and Conditions</h1>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim, nulla non sagittis efficitur, leo enim euismod ex, a auctor nibh massa porta ex. Donec mattis pellentesque maximus. Phasellus dolor tortor, luctus a eros at, faucibus pulvinar purus. In id sapien lobortis elit tincidunt efficitur. Mauris est sem, malesuada ut congue vitae, interdum a ante. Ut in orci sed libero ultrices elementum sed non nulla. Etiam hendrerit quis quam eget gravida. Suspendisse lacinia cursus ipsum vitae congue.</p>
+                <p>Morbi consequat odio at metus iaculis venenatis. In hac habitasse platea dictumst. Etiam non elit sed velit pellentesque pharetra. Sed dignissim lacus imperdiet facilisis aliquam. Praesent ornare rutrum fringilla. Sed eu ipsum eget lectus bibendum lobortis. In nec ex ullamcorper dolor vestibulum gravida. Nunc et magna neque. Ut lacus massa, elementum et dapibus porttitor, eleifend a erat. Ut imperdiet mi nibh. Proin ullamcorper lacinia libero. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas ullamcorper quam sed velit semper, sed accumsan metus interdum. Curabitur vel mi id mi hendrerit vulputate sed vel dolor. Donec sed mi leo.</p>
+                <p>Vestibulum vel blandit nisi. Mauris eget volutpat metus. Aenean eleifend ex eget sodales rutrum. Donec vitae mauris vitae ante feugiat dignissim. Fusce lacinia placerat euismod. In finibus ligula ante, sed congue libero feugiat eu. Proin mauris sapien, finibus vitae accumsan non, consectetur vel libero. In eleifend rhoncus augue, vel eleifend elit euismod vehicula. Fusce efficitur, urna ac rutrum bibendum, lacus nulla placerat augue, at interdum nisl sapien id metus. Sed vestibulum diam non enim tincidunt convallis sed facilisis enim. In condimentum, sapien in pulvinar venenatis, orci nisl blandit ligula, ac tristique dolor eros a nisl. Fusce id erat velit.</p>
+                <p>Nullam aliquam lacus magna, et eleifend turpis convallis at. Curabitur accumsan pharetra lorem convallis ornare. Integer eleifend est leo, et auctor mi lacinia ut. Aliquam in semper tortor. In et blandit dolor, nec dignissim nulla. Nulla facilisi. Vivamus elementum elit a ante vehicula ornare.</p>
+                <p>Pellentesque bibendum eros at tortor ornare cursus. Quisque sagittis lorem ut justo molestie, a auctor nisi faucibus. Maecenas suscipit vel sem non volutpat. Aliquam erat volutpat. Aenean tortor ipsum, efficitur at aliquet at, porttitor quis dolor. Donec sollicitudin venenatis dolor, et interdum velit. Aliquam viverra mi at turpis commodo dictum. Proin tellus tortor, venenatis at risus aliquam, rutrum ornare dolor. Fusce facilisis eleifend purus, vitae tempus urna vehicula ac. Mauris sodales consectetur mauris ac tempus. Donec scelerisque efficitur justo. Proin blandit metus nec risus placerat sollicitudin.</p>
+
+            </Layout>
+        </div>
+    );
+}
+
+export default Terms;
