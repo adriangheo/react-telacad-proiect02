259dd7ae70273ceda7cfb32742b93f17d5ed1345
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-07-27 19:17:33 +0300

    display favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -20,6 +20,49 @@ function Favorites(props){
             className="cart-page container-fluid container-min-max-width d-flex flex-column justify-content-center align-items-center"
             style={{background: "MediumTurquoise"}}
             >
+
+                {
+                    props.favProducts.length
+                    ? <div className="w-100">
+                        <div className="d-flex justify-content-between text-center h4 text-bold">
+                            <p className="w-25">Produs</p>
+                            <p className="w-25">Pret</p>
+                            <p className="w-25">Cantitate</p>
+                            <p className="w-25">Total</p>
+                        </div>
+                        {
+                            props.favProducts.map(product => {
+                                return <div className="d-flex justify-content-between align-items-center text-center" key={product.id}>
+                                    <div className="w-25 d-flex flex-column justify-content-center align-items-center">
+                                        <img src={product.image} alt="Produs"/>
+                                        <p>{ product.name }</p>
+                                    </div>
+                                    <p className="w-25">{ product.price } { product.currency }</p>
+                                    <p className="w-25">{ product.quantity }</p>
+                                    <div className="w-25 d-flex justify-content-center">
+                                        <p className="mr-2">{ product.price * product.quantity } { product.currency }</p>
+                                        <div onClick={() => props.removeFromCart({id: product.id})}>
+                                            <Close />
+                                        </div>
+                                    </div>
+                                </div>
+                            })
+                        }
+                        <div className="d-flex justify-content-end border-top">
+                            <div className="w-25 d-flex align-items-center justify-content-center">
+                                <p className="my-4 text-center font-weight-bold">Total de plată: </p>
+                            </div>
+                            <div className="w-25">
+                                <p className="my-4 text-center">
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    : <div className="d-flex flex-column align-items-center">
+                        <p className="h3">Nu ai produse în coș!</p>
+                        <Link to="/"><button className="btn btn-outline-dark">Inapoi la home</button></Link>
+                    </div>
+                }
             
                 
             </div>
@@ -31,16 +74,16 @@ function Favorites(props){
 function mapStateToProps(state) {
     console.log("--------------");
     console.log("state.favorites.favProducts.length:", state.favorites);
-    console.log("state.cart.products.length:", state.favorites);
+    console.log("state.cart.favProducts.length:", state.favorites);
     console.log("--------------");
     console.log("state.favorites:", state.favorites);
     console.log("state.favorites.favProducts:", state.favorites.favProducts);
     
     console.log("state ------:", state)
     return {
-        numberOfProducts: state.cart.products.length,
         user: state.user.data,
         numberOfFavorites: state.favorites.favProducts.length,
+        favProducts: state.favorites.favProducts
     }
 }
 
